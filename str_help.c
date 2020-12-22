8d2e8b8e26330feaed51e2cb05f673183c5a5e69
alexnsweet <alexnsweet@gmail.com>
2020-12-22 18:45:27 +0000

    string helper functions

--- str_help.c
+++ str_help.c
@@ -43,4 +43,55 @@ char **tokenizer(char *str, char *delim)
 		return (buffer);
 }
 
+/**
+ * _strlen - Counts the number of chars in a string
+ * @s: string to count
+ *
+ * Return: number of characters in a string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
 
+	while (s[i] != '\0')
+	{	i++;	}
+
+	return (i);
+}
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: 0 if same or < > if s1 or s2 is less or greater
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0')
+		{
+		return (0);
+		}
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+/**
+ * print_string - writes number of chars in string
+ * @str: string to print
+ *
+ * Return: -1 if error (errno is set) or # of bytes written on success
+ */
+int print_string(char *str)
+{
+	int i = 0;
+
+	i = write(1, str, _strlen(str));
+
+	return (i);
+}
