a8ac60056f1cc7dcbab02fa25f77a15292e39cde
alexnsweet <alexnsweet@gmail.com>
2020-12-22 18:53:04 +0000

    modification of isbuiltin function

---
+++ helper_funcs.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * iscommand - checks line input to see if it's a monty command
+ * @tokens: tokenized line arguments
+ * @buffer: line argument
+ *
+ * Return: 0 if not a builtin or 2 if builtin
+ */
+int iscommand(char **tokens, char *buffer)
+{
+	int i;
+
+	if (_strcmp(tokens[0], "exit") == 0)
+	{
+		free(tokens);
+		free(buffer);
+		_exit(0);
+	}
+	else if (_strcmp(tokens[0], "push") == 0)
+	{
+
+		print_string(tokens[1]);
+		print_string("\n");
+		return (2);
+	}
+
+		return (0);
+}
